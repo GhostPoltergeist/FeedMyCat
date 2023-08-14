fab93e3f6f8742c2135df0a46d186bab46cb8e4f
Harold Edsel Cabaluna <39079376+GhostPoltergeist@users.noreply.github.com>
2023-08-14 22:16:30 +0800

    adding panels

--- src/main/java/feed/cat/main/Cat.java
+++ src/main/java/feed/cat/main/Cat.java
@@ -6,13 +6,23 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.URL;
 
-import static fontLoader.FontLoader.jetBrainsMono;
-import static fontLoader.FontLoader.loadFonts;
+import static fontLoader.FontLoader.*;
 
 public class Cat extends JFrame implements KeyListener {
     JButton buttonOne;
+    JPanel rightPanel, leftPanel;
     public Cat() {
         loadFonts();
+
+        rightPanel = new JPanel();
+        rightPanel.setBackground(new Color(0xE072A4));
+        rightPanel.setBounds(290, 0, 200, 800);
+        rightPanel.setFont(jetBrainsMono.deriveFont(12f));
+
+        leftPanel = new JPanel();
+        leftPanel.setBackground(new Color(0x29AB87));
+        leftPanel.setBounds(0, 0, 220, 800);
+        leftPanel.setFont(jetBrainsMono.deriveFont(12f));
 
         URL homeButton = getClass().getClassLoader().getResource("home.gif");
         ImageIcon icon = new ImageIcon(new ImageIcon(homeButton).getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT));
@@ -30,11 +40,18 @@ public class Cat extends JFrame implements KeyListener {
 
         setSize(500, 800);
         setVisible(true);
+
+        getRootPane().putClientProperty("JRootPane.titleBarBackground", new Color(0xFFFFFF));
+        getRootPane().putClientProperty("JRootPane.titleBarForeground", Color.BLACK);
+        getRootPane().getContentPane().setBackground(new Color(0xF1FFFA));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
         setLayout(null);
+
         add(buttonOne);
+        add(rightPanel);
+        add(leftPanel);
     }
 
     @Override
--- src/main/java/feed/cat/main/Main.java
+++ src/main/java/feed/cat/main/Main.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            FlatDarkLaf.setup();
+            FlatLightLaf.setup();
             new Cat();
         });
     }
