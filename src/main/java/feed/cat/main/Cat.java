9503e48a3df56c1754a0c48dc330fb0575d207a1
Harold Edsel Cabaluna <39079376+GhostPoltergeist@users.noreply.github.com>
2023-08-14 22:45:30 +0800

    adding new  {designs and buttons}👀😶‍🌫️

--- src/main/java/feed/cat/main/Cat.java
+++ src/main/java/feed/cat/main/Cat.java
@@ -10,14 +10,57 @@ import static fontLoader.FontLoader.*;
 
 public class Cat extends JFrame implements KeyListener {
     JButton buttonOne;
+    JButton start, save, settings, about;
     JPanel rightPanel, leftPanel;
+    JLabel catTitle;
     public Cat() {
         loadFonts();
 
-        rightPanel = new JPanel();
-        rightPanel.setBackground(new Color(0xE072A4));
-        rightPanel.setBounds(290, 0, 200, 800);
-        rightPanel.setFont(jetBrainsMono.deriveFont(12f));
+        start = new JButton("Start");
+        start.setEnabled(true);
+        start.setVisible(true);
+        start.addKeyListener(this);
+        start.setFont(jetBrainsMono.deriveFont(12f));
+        start.setEnabled(true);
+        start.setBounds(17,200, 100,40);
+        start.setBackground(new Color(250, 160, 160));
+        start.setForeground(Color.WHITE);
+
+        save = new JButton("Save");
+        save.setEnabled(true);
+        save.setVisible(true);
+        save.addKeyListener(this);
+        save.setFont(jetBrainsMono.deriveFont(12f));
+        save.setEnabled(true);
+        save.setBounds(17,260, 100,40);
+        save.setBackground(new Color(250, 160, 160));
+        save.setForeground(Color.WHITE);
+
+        settings = new JButton("Settings");
+        settings.setEnabled(true);
+        settings.setVisible(true);
+        settings.addKeyListener(this);
+        settings.setFont(jetBrainsMono.deriveFont(12f));
+        settings.setEnabled(true);
+        settings.setBounds(17,320, 100,40);
+        settings.setBackground(new Color(250, 160, 160));
+        settings.setForeground(Color.WHITE);
+
+        about = new JButton("About");
+        about.setEnabled(true);
+        about.setVisible(true);
+        about.addKeyListener(this);
+        about.setFont(jetBrainsMono.deriveFont(12f));
+        about.setEnabled(true);
+        about.setBounds(17,380, 100,40);
+        about.setBackground(new Color(250, 160, 160));
+        about.setForeground(Color.WHITE);
+
+        URL cat = getClass().getClassLoader().getResource("cat.gif");
+        ImageIcon myCat = new ImageIcon(new ImageIcon(cat).getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT));
+
+        catTitle = new JLabel(myCat);
+        catTitle.setBounds(14,10, 200, 200);
 
         leftPanel = new JPanel();
         leftPanel.setBackground(new Color(0x29AB87));
@@ -36,7 +79,14 @@ public class Cat extends JFrame implements KeyListener {
         buttonOne.setBorderPainted(false);
         buttonOne.setFont(jetBrainsMono.deriveFont(12f));
         buttonOne.setEnabled(true);
-        buttonOne.setBounds(320,0, 200,200);
+        buttonOne.setBounds(7,0, 200,200);
+
+        rightPanel = new JPanel();
+        rightPanel.setBackground(new Color(0xE072A4));
+        rightPanel.setBounds(290, 0, 200, 800);
+        rightPanel.setFont(jetBrainsMono.deriveFont(12f));
+        rightPanel.setLayout(null);
+        rightPanel.add(buttonOne);
 
         setSize(500, 800);
         setVisible(true);
@@ -49,7 +99,13 @@ public class Cat extends JFrame implements KeyListener {
         setResizable(false);
         setLayout(null);
 
-        add(buttonOne);
+        add(catTitle);
+
+        add(start);
+        add(save);
+        add(settings);
+        add(about);
+
         add(rightPanel);
         add(leftPanel);
     }
